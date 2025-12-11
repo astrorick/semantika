8ed4c90bd1633bd9ee37e3131758cb33c5ee8576
Astrorick <me@astrorick.space>
2025-12-11 17:36:46 +0100

    Minor changes to README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Semantika
 
-_Semantika_ is a simple implementation of semantic versioning in the Go programming language.
+_Semantika_ is a Go package that provides a simple but strict implementation of semantic versioning in Go, based on the [Semantic Versioning 2.0.0](https://semver.org/) specification.
--- semantika.go
+++ semantika.go
@@ -1,4 +1,4 @@
-// Semantika is a Go package that provides a simple but strict implementation of semantic versioning in Go.
+// Semantika is a Go package that provides a simple but strict implementation of semantic versioning in Go, ased on the [Semantic Versioning 2.0.0](https://semver.org/) specification.
 package semantika
 
 import (
